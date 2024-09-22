c96610dfaadfa280921da0aed9ef7fa9660bab83
drew <github@andbrant.com>
2024-09-22 17:59:34 -0500

    change disk for setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-rm -rf /run/media/drew/1B60-E60F/* ; cp -ar /home/drew/playin/wz_mini_hacks/SD_ROOT/* /run/media/drew/1B60-E60F/
+rm -rf /run/media/drew/E713-DA28/* ; cp -ar /home/drew/playin/wz_mini_hacks/SD_ROOT/* /run/media/drew/E713-DA28/
